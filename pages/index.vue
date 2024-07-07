d56e8a424022df86166a2bfacf0906cb22e8c404
JeeJee <jjkwanhere@gmail.com>
2024-07-07 22:39:41 +0800

    chore: Add Download icon to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Keynote from "~/assests/svg/Keynote.svg";
+import Download from "~/assests/svg/Download.svg";
 </script>
 <template>
   <div class="flex items-center space-x-4 p-4">
@@ -8,6 +9,9 @@ import Keynote from "~/assests/svg/Keynote.svg";
     <Button variant="outline" size="medium">
       Button
       <Keynote class="ml-1 h-4 w-4 fill-gray-950" />
+    </Button>
+    <Button variant="outline" size="icon">
+      <Download class="h-4 w-4 fill-gray-950" />
     </Button>
   </div>
 </template>
