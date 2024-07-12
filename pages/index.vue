0952d8b5883e9a2f8395e26e0c29c701a0dc845f
JeeJee <jjkwanhere@gmail.com>
2024-07-12 22:52:13 +0800

    chore: Add duplicate Button component to index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
 <template>
   <div class="flex items-center space-x-4 p-4">
     <Button variant="primary" size="medium" class="bg-orange-500" as-child><a href="/">A Button</a></Button>
+    <Button variant="primary" size="medium" class="bg-orange-500" as-child><a href="/">A Button</a></Button>
   </div>
 </template>
