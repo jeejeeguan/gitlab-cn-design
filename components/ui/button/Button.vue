edd9e1b21cc9a5d16c8b985f5cb23dea229b716f
Jeejee Guan <30567623+jeejeeguan@users.noreply.github.com>
2024-07-12 18:27:32 +0800

    chore: Refactor Button component and update utils

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -2,6 +2,7 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import type { HTMLAttributes } from "vue";
 import { Primitive, type PrimitiveProps } from "radix-vue";
+import { cn } from "~/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-base font-normal leading-normal transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts"></script>
 <template>
   <div class="flex items-center space-x-4 p-4">
-    <Button variant="primary" size="medium">Button</Button>
-    <Button variant="primary" size="medium" as-child>
-      <a href="/">Button</a>
-    </Button>
+    <Button variant="primary" size="medium" class="bg-orange-500" as-child><a href="/">A Button</a></Button>
   </div>
 </template>
