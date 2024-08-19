8cc054c48a6fd8874928825dcc8348a89138d306
JeeJee <jjkwanhere@gmail.com>
2024-08-19 23:04:44 +0800

    Update ESLint configuration to add Vue self-closing tag rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+          component: "always",
+        },
+      },
+    ],
+  },
+});
