25ce23d9b00af104018614951f89b0626105332d
Jeejee Guan <30567623+jeejeeguan@users.noreply.github.com>
2024-08-21 14:15:00 +0800

    Add button component of lg size

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -11,20 +11,21 @@ const buttonVariants = cva(
       variant: {
         // sm:hover 确保 hover 样式只在非 mobile 尺寸的浏览器上生效，避免了 hover 样式不清除的问题
         // active 确保了 mobile 尺寸的浏览器上用户点按的时候仍然有交互样式
-        primary: "bg-gray-950 text-white focus-visible:ring-gray-950 sm:hover:bg-gray-700 active:bg-gray-700",
+        primary: "bg-orange-600 text-white focus-visible:ring-orange-600 sm:hover:bg-orange-500 active:bg-orange-500",
+        secondary: "bg-gray-950 text-white focus-visible:ring-gray-950 sm:hover:bg-gray-700 active:bg-gray-700",
         outline:
           "bg-white/0 text-gray-950 border border-gray-300 focus-visible:ring-gray-950 sm:hover:bg-gray-100 active:bg-gray-100",
         ghost: "bg-white/0 text-gray-950 focus-visible:ring-gray-950 sm:hover:bg-gray-100 active:bg-gray-100",
         link: "text-gray-950 sm:hover:underline underline-offset-4 focus-visible:ring-gray-950 active:underline",
       },
       size: {
-        medium: "h-10 px-4 py-2",
-        icon: "h-8 w-8",
+        md: "h-10 px-4",
+        lg: "h-12 px-6",
       },
     },
     defaultVariants: {
       variant: "primary",
-      size: "medium",
+      size: "md",
     },
   },
 );
@@ -39,7 +40,7 @@ interface Props extends PrimitiveProps {
 
 const props = withDefaults(defineProps<Props>(), {
   variant: "primary",
-  size: "medium",
+  size: "md",
   class: "",
   as: "button",
 });
@@ -47,6 +48,6 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <Primitive :as="as" :as-child="asChild" :class="cn(buttonVariants({ variant, size }), props.class)">
-    <slot />
+    <slot></slot>
   </Primitive>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,7 @@
-<script setup>
-import Search from "~/assets/svg/Search.svg";
-</script>
+<script setup></script>
 <template>
   <div class="flex h-screen w-full space-x-4 p-4">
-    <Button size="medium" variant="primary">Search</Button>
-
-    <Button size="medium" variant="primary">
-      Link Button
-      <Search class="ml-1 h-4 w-4 fill-white" />
-    </Button>
-
-    <Button size="medium" variant="link">Link Button</Button>
-
-    <Button size="medium" variant="link">
-      Link Button
-      <Search class="ml-1 h-4 w-4 fill-gray-950" />
-    </Button>
+    <Button size="md" variant="secondary">Button</Button>
+    <Button size="lg" variant="primary">Button</Button>
   </div>
 </template>
